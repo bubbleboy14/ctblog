46c1bc5d58815782536b215be6b8b7d45d91a4d3
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-27 16:14:01 -0700

    blog.core.util.comment()

--- ctblog/js/blog/core/util.js
+++ ctblog/js/blog/core/util.js
@@ -1,4 +1,5 @@
 blog.core.util = {
+	_newPost: "<b>New " + core.config.ctblog.post.name + "</b>",
 	post: function() {
 		(new CT.dom.Modal({
 			content: [
@@ -6,6 +7,10 @@ blog.core.util = {
 				// etc....
 			]
 		})).show()
+	},
+	comment: function(c) {
+		return CT.dom.node("<b>" + CT.data.get(c.user).firstName + "</b>: " + c.body,
+			"div", "margined padded bordered round");
 	}
 };
 blog.core.util._user = user.core.get();
