a76412dc7cf10630993788e0bc43377f2460c1a2
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-27 16:16:44 -0700

    post: submission (new post) and editing

--- ctblog/js/blog/pages/post.js
+++ ctblog/js/blog/pages/post.js
@@ -10,36 +10,57 @@ CT.onload(function() {
 	var content = CT.dom.node(null, null, "ctcontent"),
 		tlist = CT.dom.node(null, null, "ctlist"),
 		blurs = core.config.ctblog.post.blurs,
-		title = CT.dom.smartField({ blurs: blurs.title, classname: "w1"}),
-		blurb = CT.dom.smartField({ blurs: blurs.blurb, classname: "w1 h200p", isTA: true }),
-		body = CT.dom.smartField({ blurs: blurb.body, classname: "w1 h500p", isTA: true }),
-		submit = CT.dom.button("Submit", function() {
-			if (!title.value || !blurb.value || !body.value)
-				return alert("please complete all fields");
-			var pdata = {
-				user: blog.core.util._user.key,
-				title: title.value,
-				blurb: blurb.value,
-				body: body.value
-			};
-			CT.net.post("/_blog", CT.merge({
-				action: "post"
-			}, pdata), null, function(key) {
-				pdata.key = key;
-				pdata.label = pdata.title;
-				var t = CT.panel.trigger(pdata, viewPost);
-				if (tlist.firstChild.nextSibling)
-					tlist.insertBefore(t, tlist.firstChild.nextSibling);
-				else
-					tlist.appendChild(t);
-				t.trigger();
-			});
-		}), viewPost = function(p) {
-			CT.dom.setContent(content, CT.dom.node([ title, blurb,
-				body, submit ], "div", "bordered padded round"));
+		editPost = function(p) {
+			if (p.label == blog.core.util._newPost)
+				return CT.db.withSchema(function(schema) {
+					editPost(CT.merge(schema.post));
+				});
+			var title = CT.dom.smartField({ blurs: blurs.title, value: p.title, classname: "w1" }),
+				blurb = CT.dom.smartField({ blurs: blurs.blurb, value: p.blurb, classname: "w1 h100p", isTA: true }),
+				body = CT.dom.smartField({ blurs: blurs.body, value: p.body, classname: "w1 h400p", isTA: true }),
+				live = CT.dom.checkboxAndLabel("Go Live", p.live, null, "pointer", "right");
+			CT.dom.setContent(content, CT.dom.node([title, blurb, body, live, CT.dom.button("Submit", function() {
+				if (!title.value || !blurb.value || !body.value)
+					return alert("please complete all fields");
+				var pdata = {
+					user: blog.core.util._user.key,
+					title: title.value,
+					blurb: blurb.value,
+					body: body.value,
+					live: live.firstChild.checked
+				};
+				CT.net.post("/_blog", CT.merge({
+					action: "post",
+					key: p.key
+				}, pdata), null, function(key) {
+					var d = CT.data.get(key);
+					if (!d) {
+						d = pdata;
+						d.key = key;
+						d.label = d.title;
+						CT.data.add(d);
+						var t = CT.panel.trigger(d, editPost);
+						if (tlist.firstChild.nextSibling)
+							tlist.insertBefore(t, tlist.firstChild.nextSibling);
+						else
+							tlist.appendChild(t);
+						t.trigger();
+					} else {
+						for (var pd in pdata)
+							p[pd] = pdata[pd];
+						if (p.title != p.label)
+							p.node.rename(p.title);
+					}
+				});
+			})], "div", "bordered padded round"));
 		};
 	blog.core.db.posts(function(posts) {
-		CT.panel.triggerList(posts, viewPost, tlist);
+		CT.data.addSet(posts);
+		posts.unshift({
+			label: blog.core.util._newPost
+		});
+		CT.panel.triggerList(posts, editPost, tlist);
+		tlist.firstChild.trigger();
 		CT.dom.setContent("ctmain", [ tlist, content ]);
 	}, false, true);
 });
