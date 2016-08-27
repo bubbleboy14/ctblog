19e505abdbc9bc06d331a259f9344877a1250dda
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-27 16:15:37 -0700

    blog: omit user requirement; add comments (including submission)

--- ctblog/js/blog/pages/blog.js
+++ ctblog/js/blog/pages/blog.js
@@ -2,18 +2,47 @@ CT.require("CT.all");
 CT.require("core");
 CT.require("user.core");
 CT.require("blog.core");
-if (!blog.core.util._user)
-	location = "/";
 
 CT.onload(function() {
 	CT.initCore();
 	blog.core.db.posts(function(posts) {
-		CT.dom.setContent("ctmain", post.map(function(p) {
+		CT.dom.setContent("ctmain", posts.map(function(p) {
+			var cnode = CT.dom.node();
+			blog.core.db.comments(function(comments) {
+				CT.db.multi(comments.map(function(c) {
+					return c.user;
+				}), function() {
+					var content = [
+						CT.dom.node("Comments", "div", "bigger bold"),
+						comments.map(blog.core.util.comment)
+					];
+					if (blog.core.util._user)
+						content.push([
+							CT.dom.smartField({
+								classname: "w1",
+								blurs: core.config.ctblog.blog.blurs.comment,
+								cb: function(val) {
+									var pdata = {
+										action: "comment",
+										user: blog.core.util._user.key,
+										post: p.key,
+										body: val
+									};
+									val && CT.net.post("/_blog", pdata, null, function() {
+										cnode.firstChild.firstChild.nextSibling.appendChild(blog.core.util.comment(pdata));
+									});
+								}
+							})
+						]);
+					CT.dom.setContent(cnode, content);
+				});
+			}, p);
 			return CT.dom.node([
 				CT.dom.node(p.title, "div", "biggest bold padded"),
 				CT.dom.img(p.img, "w1"),
 				CT.dom.node(p.blurb, "div", "gray italic blockquote"),
-				CT.dom.node(p.body, "div", "padded")
+				CT.dom.node(p.body, "div", "padded"),
+				cnode
 			], "div", "bordered padded round");
 		}));
 	}, true);
