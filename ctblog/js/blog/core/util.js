2b3aec84fd489be32fe7511a43f18cd7726dd9db
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-30 16:52:56 -0700

    blog.core.util.comment(): include user img/link

--- ctblog/js/blog/core/util.js
+++ ctblog/js/blog/core/util.js
@@ -1,6 +1,6 @@
 blog.core.util = {
 	_newPost: "<b>New " + core.config.ctblog.post.name + "</b>",
-	post: function() {
+	post: function() { // TODO: fill this out! (steal from pages/post)
 		(new CT.dom.Modal({
 			content: [
 				CT.dom.node("Submit Your " + core.config.post.name),
@@ -9,8 +9,15 @@ blog.core.util = {
 		})).show()
 	},
 	comment: function(c) {
-		return CT.dom.node("<b>" + CT.data.get(c.user).firstName + "</b>: " + c.body,
-			"div", "margined padded bordered round");
+		var u = CT.data.get(c.user);
+		return CT.dom.node([
+			CT.dom.link([
+				CT.dom.img(u.img, "h50 vmiddle"),
+				CT.dom.pad(),
+				CT.dom.node(u.firstName, "b", "h50 round hoverglow")
+			], null, "/user/profile.html#" + u.key),
+			CT.dom.node(": " + c.body, "span")
+		], "div", "margined padded bordered round");
 	}
 };
 blog.core.util._user = user.core.get();
