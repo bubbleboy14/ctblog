ebd0caa656a1e727241c199409ca3d3ff74eda94
bubbleboy14 <mario.balibrera@gmail.com>
2016-11-22 14:05:37 -0800

    model: VideoPost and new base class (shared wih Post), BasePost

--- ctblog/model.py
+++ ctblog/model.py
@@ -1,14 +1,19 @@
 from cantools import db
 
-class Post(db.TimeStampedBase):
+class BasePost(db.TimeStampedBase):
 	user = db.ForeignKey(kind="ctuser")
 	live = db.Boolean(default=False)
 	title = db.String()
 	blurb = db.String()
+
+class Post(BasePost):
+	img = db.Binary()	
 	body = db.Text()
-	img = db.Binary()
+
+class VideoPost(BasePost):
+	video = db.Binary()
 
 class Comment(db.TimeStampedBase):
 	user = db.ForeignKey(kind="ctuser")
-	post = db.ForeignKey(kind="post")
+	post = db.ForeignKey(kinds=["post", "videopost"])
 	body = db.Text()
