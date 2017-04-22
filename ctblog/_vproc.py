8887cc49f3df665f789972da0c882b7855e57c7d
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-21 21:52:22 -0700

    /_vproc handler: use new hlsify() ct function

--- ctblog/_vproc.py
+++ ctblog/_vproc.py
@@ -1,19 +1,9 @@
-import os
 from cantools.web import respond, cgi_get, succeed
-from cantools.util import log, mkdir, segment
+from cantools.util import hlsify
 from model import db
 
 def response():
-	v = db.get(cgi_get("v"))
-	orig = v.video.urlsafe()[1:]
-	p = orig.replace("blob/", "blob/hls/")
-	isd = os.path.isdir(p)
-	if cgi_get("check", default=False):
-		succeed(isd)
-	log("transcode > attempt with video: '%s'"%(v.title,), important=True)
-	if not isd:
-		mkdir(p, True)
-		segment(orig, p)
-		log("transcode > done!")
+	succeed(hlsify(db.get(cgi_get("v")).video.urlsafe()[1:],
+		check=cgi_get("check", default=False)))
 
 respond(response, threaded=True)
