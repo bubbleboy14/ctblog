36865f97b0be246fdbee1c19273c273f51657026
bubbleboy14 <mario.balibrera@gmail.com>
2017-05-22 17:37:01 -0700

    main blog page: direct linking to stories via link tab (copy to clipboard) and startFrame (from hash)

--- ctblog/js/blog/pages/blog.js
+++ ctblog/js/blog/pages/blog.js
@@ -8,14 +8,29 @@ CT.onload(function() {
 	blog.core.db.posts(function(posts) {
 		posts.reverse();
 		if (core.config.ctblog.post.tabbed) {
-			new CT.slider.Slider({
+			var slider = new CT.slider.Slider({
 				frames: posts,
 				noStyle: true,
 				autoSlide: false,
 				parent: "ctmain",
 				arrowPosition: "bottom",
 				bubblePosition: "bottom",
-				frameCb: blog.core.util.post
+				frameCb: blog.core.util.post,
+				tab: {
+					origin: "topright",
+					content: CT.dom.img({
+						src: "/img/clipboard.png",
+						style: {
+							width: "60px",
+							padding: "15px 15px 0px 0px"
+						},
+						onclick: function() {
+							var h = encodeURIComponent(posts[slider.index].label);
+							CT.clipboard(document.location + "#" + h);
+						}
+					})
+				},
+				startFrame: decodeURIComponent(document.location.hash.slice(1))
 			});
 		} else
 			CT.dom.setContent("ctmain", posts.map(blog.core.util.post));
