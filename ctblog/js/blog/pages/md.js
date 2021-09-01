f8a91d7b6198e225c78976a6ae761d0542139f57
bubbleboy14 <mario.balibrera@gmail.com>
2021-08-31 18:42:08 -0700

    md page loader: support video mode (via CT.parse.enableVideo()) and poetry mode (via proc()/poetrize()/poLine())

--- ctblog/js/blog/pages/md.js
+++ ctblog/js/blog/pages/md.js
@@ -1,8 +1,21 @@
 CT.require("CT.all");
 CT.require("core");
 CT.scriptImport("https://cdn.jsdelivr.net/npm/marked/marked.min.js");
+var cfg = core.config.ctblog.md;
+
+var poLine = function(line) {
+	if (line.startsWith("#"))
+		return line;
+	return line + "\\";
+};
+
+var poetrize = function(text) {
+	return text.split("\n").map(poLine).join("\n").slice(0, -1);
+};
 
 var proc = function(text) {
+	if (cfg.poetry)
+		text = poetrize(text);
 	return marked(text.replace(/\n\n"""\n/g,
 		"<div class='big blockquote'>").replace(/\n"""\n\n/g, "</div>"));
 };
@@ -13,7 +26,7 @@ var ytFix = function(iframe) {
 
 CT.onload(function() {
 	CT.initCore();
-	CT.parse.enableVideo();
+	cfg.video && CT.parse.enableVideo();
 	var h = location.hash.slice(1);
 	fetch("/md/" + h + ".md").then(d => d.text()).then(function(text) {
 		if (text.startsWith("<b>404</b>"))
