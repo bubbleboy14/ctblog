350ebca5e2d903d2666fe4656c4cd26d3385fd55
bubbleboy14 <mario.balibrera@gmail.com>
2022-07-06 07:45:47 -0700

    /_vproc: vpath; mp4 transcode() w/o slow

--- ctblog/_vproc.py
+++ ctblog/_vproc.py
@@ -4,13 +4,18 @@ from model import db
 
 def response():
 	action = cgi_get("action")
-	vid = db.get(cgi_get("v")).video
-	if not vid: # no vid to process
-		succeed()
-	vpath = vid.urlsafe()[1:]
+	vpath = cgi_get("vpath", required=False)
+	if not vpath:
+		vid = db.get(cgi_get("v")).video
+		if not vid: # no vid to process
+			succeed()
+		vpath = vid.urlsafe()
+	elif ".." in vpath or not vpath.startswith("/v/"): # TODO: configurize ranvid path
+		succeed() # invalid ranvid path
+	vpath = vpath[1:]
 	if action == "hls":
 		succeed(hlsify(vpath, check=cgi_get("check", default=False)))
 	elif action == "mp4":
-		transcode(vpath, slow=True)
+		transcode(vpath)
 
 respond(response, threaded=True)
