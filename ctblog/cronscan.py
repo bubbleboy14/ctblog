0d348a470b599f24fc319cf77b926fb10122b548
bubbleboy14 <mario.balibrera@gmail.com>
2022-07-08 14:24:41 -0700

    cronscan - Moover: if config.moov, scan /v/ (via shouldMoveMoov()) and transcode() as necessary

--- ctblog/cronscan.py
+++ ctblog/cronscan.py
@@ -1,8 +1,28 @@
+import os
 from datetime import datetime, timedelta
 from cantools.web import respond
-from cantools.util import log, hlsify
+from cantools.util import log, output, transcode, shouldMoveMoov, hlsify
 from cantools import config
 from model import *
+
+class Moover(object):
+	def __init__(self):
+		self.checked = set()
+
+	def check(self, fname):
+		fpath = "v/%s"%(fname,)
+		shouldMoveMoov(fpath) and transcode(fpath)
+		self.checked.add(fname)
+
+	def __call__(self):
+		for dn, dz, fz in os.walk("v"):
+			log("Moover scanning %s video files (%s already checked)"%(len(fz), len(self.checked)))
+			for f in fz:
+				if f not in self.checked:
+					self.check(f)
+			return
+
+moover = Moover()
 
 def response():
 	log("initiating cronscan", important=True)
@@ -20,6 +40,8 @@ def response():
 		log("scanning %s VideoPosts for missing hls files"%(len(vz),), 1)
 		for v in vz:
 			hlsify(v.video.urlsafe()[1:])
+	if config.moov:
+		moover()
 	log("cronscan complete")
 
 respond(response)
