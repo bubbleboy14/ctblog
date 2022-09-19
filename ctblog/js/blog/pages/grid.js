306073eb2133c0366a8cc8f3ab6709e92683c6ac
bubbleboy14 <mario.balibrera@gmail.com>
2022-09-19 12:45:45 -0700

    grid page loader: support "i" mode

--- ctblog/js/blog/pages/grid.js
+++ ctblog/js/blog/pages/grid.js
@@ -3,25 +3,39 @@ CT.require("core");
 CT.require("user.core");
 CT.require("blog.ranvid");
 
-var vidz = CT.dom.div(null, "centered");
+var mode = location.hash.slice(1) || "v"; // || i
+var cont = CT.dom.div(null, "centered");
 var vid = function(v) {
-	CT.dom.addContent(vidz, CT.dom.video({
+	CT.dom.addContent(cont, CT.dom.video({
 		src: v,
 		controls: true,
 		className: "h200p inline-block"
 	}));
 };
 var refill = function(num) {
-	num = num || 6;
-	for (var i = 0; i < num; i++)
-		blog.ranvid.get(vid);
+	num = num || 10;
+	if (mode == "i") {
+		CT.net.post({
+			path: "/_blog",
+			params: {
+				action: "imgz",
+				count: num
+			},
+			cb: function(iz) {
+				iz.map(i => CT.dom.addContent(cont,
+					CT.dom.img("/img/z/" + i, "h200p")))
+			}
+		});
+	} else
+		for (var i = 0; i < num; i++)
+			blog.ranvid.get(vid);
 };
 
 CT.onload(function() {
 	CT.initCore();
-	refill(12);
+	refill(20);
 	CT.dom.setMain([
-		vidz,
+		cont,
 		CT.dom.div({
 			content: "loading...",
 			onvisible: refill
