4b4ef3974c3c688121af6607ff271aff2c896bef
bubbleboy14 <mario.balibrera@gmail.com>
2022-12-12 18:04:52 -0800

    blog.core.db: posts() and latest() support variety (overrides core.config.ctblog.post.mode)

--- ctblog/js/blog/core/db.js
+++ ctblog/js/blog/core/db.js
@@ -2,16 +2,16 @@ blog.core.db = {
 	comments: function(cb, post) {
 		CT.db.get("comment", cb, null, null, null, { post: post.key });
 	},
-	posts: function(cb, live, myposts, filters, order, limit, offset, sync) {
+	posts: function(cb, live, myposts, variety, filters, order, limit, offset, sync) {
 		filters = filters || {};
 		if (live)
 			filters.live = true;
 		if (myposts && blog.core.util._user)
 			filters.user = blog.core.util._user.key;
-		return CT.db.get(core.config.ctblog.post.mode, cb, limit, offset, order, filters, sync);
+		return CT.db.get(variety || core.config.ctblog.post.mode, cb, limit, offset, order, filters, sync);
 	},
-	latest: function(cb, filters, limit, offset, sync) {
-		return blog.core.db.posts(cb, true, false,
+	latest: function(cb, variety, filters, limit, offset, sync) {
+		return blog.core.db.posts(cb, true, false, variety,
 			filters, "-created", limit || 1, offset, sync);
 	}
 };
