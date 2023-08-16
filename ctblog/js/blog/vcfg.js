04d18f91fd84f4b02a88a56c792dbe9103ab629f
bubbleboy14 <mario.balibrera@gmail.com>
2023-08-15 17:03:30 -0700

    blog.vcfg{}: _.tag(), which creates a new (CT.modal.prompt()-specified) tag, then calls cb(); vz() supports (passing to CT.net.post()) cb() and eb(), as well as (via merge() order adjustment) non-vz action; tag() multiple-select (as well as tag creation via _.tag()) modal.prompt() generator; init() acquires (via CT.db.get()) tags[] before calling bv.vz(bv.build). blog.vcfg.Tagger: edit(), which (via bv.tag() and saver()) sets vid.tags[]; node() single-tag node generator; tags() compound view/mod interface generator; constructor, which simply sets opts{} and saver(). blog.vcfg.Browser: constructor instantiates Tagger; view() uses tagger.tags(); saver() passes selective (as d{} becomes polluted) eopts{} to bv.vz()

--- ctblog/js/blog/vcfg.js
+++ ctblog/js/blog/vcfg.js
@@ -1,12 +1,44 @@
 blog.vcfg = {
-	_: {},
-	vz: function(cb, params) {
+	_: {
+		tag: function(cb) {
+			var bv = blog.vcfg, _ = bv._;
+			CT.modal.prompt({
+				prompt: "ok, what's the tag?",
+				cb: function(tname) {
+					bv.vz(function(tdata) {
+						CT.data.add(tdata);
+						_.tags.push(tdata);
+						cb();
+					}, {
+						action: "tz",
+						name: tname
+					}, cb);
+				}
+			})
+		}
+	},
+	vz: function(cb, params, eb) {
 		CT.net.post({
 			path: "/_blog",
-			params: CT.merge({
+			params: CT.merge(params, {
 				action: "vz"
-			}, params),
-			cb: cb
+			}),
+			cb: cb,
+			eb: eb
+		});
+	},
+	tag: function(d, cb) {
+		var bv = blog.vcfg, _ = bv._;
+		CT.modal.choice({
+			prompt: "please select your tags",
+			style: "multiple-choice",
+			selections: d.tags,
+			data: ["new tag"].concat(_.tags),
+			cb: function(tags) {
+				if (tags[0] == "new tag")
+					return _.tag(() => bv.tag(d, cb));
+				cb(tags.map(t => t.key));
+			}
 		});
 	},
 	build: function(videos) { // {all[],tagged[]}
@@ -15,19 +47,48 @@ blog.vcfg = {
 		});
 	},
 	init: function() {
-		blog.vcfg.vz(blog.vcfg.build);
+		var bv = blog.vcfg, _ = bv._;
+		CT.db.get("tag", function(tags) {
+			_.tags = tags;
+			bv.vz(bv.build);
+		});
 	}
 };
 
+blog.vcfg.Tagger = CT.Class({
+	edit: function(d) {
+		blog.vcfg.tag(d, tags => this.saver(d, "tags", tags));
+	},
+	node: function(tkey) {
+		return CT.dom.div(CT.data.get(tkey).name,
+			"bordered padded margined round inline-block");
+	},
+	tags: function(d) { // tags[]
+		return CT.dom.div([
+			CT.dom.button("edit", () => this.edit(d), "right"),
+			"tags",
+			d.tags.map(this.node)
+		], "bordered padded round mt10")
+	},
+	init: function(opts) {
+		this.opts = opts;
+		this.saver = opts.saver;
+	}
+});
+
 blog.vcfg.Browser = CT.Class({
 	saver: function(d, prop, val) {
-		var edited = this._.edited;
-		d[prop] = val;
+		var edited = this._.edited, eopts = {};
+		d[prop] = eopts[prop] = val;
+		if (d.key)
+			eopts.key = d.key;
+		else
+			eopts.modelName = d.modelName;
 		blog.vcfg.vz(function(upd) {
 			edited(d, upd);
 			Object.assign(d, upd);
 		}, {
-			vinfo: d
+			vinfo: eopts
 		});
 	},
 	blurber: function(d) {
@@ -39,9 +100,6 @@ blog.vcfg.Browser = CT.Class({
 			cb: val => this.saver(d, "blurb", val)
 		});
 	},
-	tagger: function(d) { // tags[]
-
-	},
 	filer: function(d) { // filename
 
 	},
@@ -49,7 +107,7 @@ blog.vcfg.Browser = CT.Class({
 		CT.dom.setContent(this._.nodes.content, [
 			this.namer(d),
 			this.blurber(d),
-			this.tagger(d),
+			this.tagger.tags(d),
 			this.filer(d)
 		]);
 	},
@@ -75,6 +133,9 @@ blog.vcfg.Browser = CT.Class({
 			allvids: vz.all,
 			owner: false // revisit?
 		}, this.opts);
-		this._.tagged = {};
+		this._.tagged = {}
+		this.tagger = new blog.vcfg.Tagger({
+			saver: this.saver
+		});
 	}
 }, CT.Browser);
