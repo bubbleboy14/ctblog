f7282432e1b23757b71fabf051ada72abf56b0d8
bubbleboy14 <mario.balibrera@gmail.com>
2023-10-18 21:49:46 -0700

    simplified dlinx now uses metize() in cantools.web.util

--- ctblog/_dlinx.py
+++ ctblog/_dlinx.py
@@ -1,27 +1,5 @@
-from dez.http.static import StaticStore
-from cantools.web import respond, local, cgi_get, send_file
+from cantools.web import respond, cgi_get, metize
 from cantools.util import read
-from cantools import config
-from model import db
-
-filestore = StaticStore()
-qcache = {}
-#<script type="application/ld+json">{"@context":"http://schema.org","@type":"Video","name":"","image":""}</script>
-METS = """
-  <meta property="og:title" content="%s">
-  <meta property="twitter:title" content="%s">
-  <meta property="og:image" content="%s">
-  <meta property="twitter:image" content="%s">
-  <meta property="og:description" content="%s">
-  <meta property="twitter:description" content="%s">
-  <meta property="description" content="%s">
-"""
-
-def metize(m):
-	n = m["name"]
-	i = m["image"]
-	b = m["blurb"]
-	return METS%(n, n, i, i, b, b, b)
 
 def striplinx(s):
 	while "](" in s:
@@ -31,40 +9,29 @@ def striplinx(s):
 		s = s[:start] + s[start + 1:mid] + s[end + 1:]
 	return "%s..."%(s.strip(".,;:"),)
 
-def response():
-	qs = local("request_string") # better key?
-	p = local("response").request.url
-#	print(p)
-	mdir = config.mode == "dynamic" and "html" or "html-%s"%(config.mode,)
-	fp = "%s%s"%(mdir, p)
-#	print(fp)
-	if p not in qcache:
-		qcache[p] = {}
-	if qs not in qcache[p]:
-		metas = None
-		markup = filestore.read(fp)[0].decode()
-		if p == "/blog/md.html":
-			chap = cgi_get("c", required=False, decode=True)
-			md = read("md/%s.md"%(cgi_get("n"),))
-			pars = md.split("\n")
-			metas = {}
-			metas["name"] = pars.pop(0)
-			if chap:
-				metas["name"] += " |%s"%(chap,)
-				chap = chap.strip()
+def mextract(p, markup):
+	if p == "/blog/md.html":
+		chap = cgi_get("c", required=False, decode=True)
+		md = read("md/%s.md"%(cgi_get("n"),))
+		pars = md.split("\n")
+		metas = {}
+		metas["name"] = pars.pop(0)
+		if chap:
+			metas["name"] += " |%s"%(chap,)
+			chap = chap.strip()
+			line = pars.pop(0)
+			while line != chap:
 				line = pars.pop(0)
-				while line != chap:
-					line = pars.pop(0)
-			pars.pop(0) # underline
-			metas["blurb"] = striplinx(pars.pop(0) or pars.pop(0))
-			for par in pars:
-				if par.startswith("https://"):
-					metas["image"] = par
-					break
-			print("\ndlinx metas:", metas, "\n\n")
-		if metas:
-			markup = markup.replace("<head>", "<head>%s"%(metize(metas),))
-		qcache[p][qs] = markup
-	send_file(qcache[p][qs])
+		pars.pop(0) # underline
+		metas["blurb"] = striplinx(pars.pop(0) or pars.pop(0))
+		for par in pars:
+			if par.startswith("https://"):
+				metas["image"] = par
+				break
+		print("\ndlinx metas:", metas, "\n\n")
+		return metas
+
+def response():
+	metize(mextract)
 
 respond(response)
