5a40fa1c5c2877c32ebf1d208bbb5981290d0573
bubbleboy14 <mario.balibrera@gmail.com>
2023-10-27 12:22:43 -0700

    dlinx derives metas[image] via text2image()

--- ctblog/_dlinx.py
+++ ctblog/_dlinx.py
@@ -1,4 +1,4 @@
-from cantools.web import respond, cgi_get, metize
+from cantools.web import respond, cgi_get, metize, text2image
 from cantools.util import read
 
 def striplinx(s):
@@ -24,12 +24,7 @@ def mextract(p, markup):
 				line = pars.pop(0)
 		pars.pop(0) # underline
 		metas["blurb"] = striplinx(pars.pop(0) or pars.pop(0))
-		for par in pars:
-			if par.startswith("https://"):
-				if "youtube.com" in par:
-					par = "https://img.youtube.com/vi/%s/0.jpg"%(par.split("?v=")[1],)
-				metas["image"] = par
-				break
+		metas["image"] = text2image(pars)
 		print("\ndlinx metas:", metas, "\n\n")
 		return metas
 
